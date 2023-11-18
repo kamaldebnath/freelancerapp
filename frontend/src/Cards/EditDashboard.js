49797d67f2bb44e7fc2c3d4e3723a70f610ad7c6
kamaldebnath <debnathk037@gmail.com>
2023-11-18 19:01:19 +0530

    latest

--- frontend/src/Cards/EditDashboard.js
+++ frontend/src/Cards/EditDashboard.js
@@ -2,18 +2,26 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-function EditDashboard({ user, uid, about, displayName }) {
+function EditDashboard({ user, uid, about, displayName,age,gender,github,linkedin }) {
 
     const backend_url = 'https://freelancerapp-wdtf.onrender.com';
     const navigate = useNavigate();
     const [newAbout, setNewAbout] = useState();
     const [newName, setNewName] = useState();
+    const [newAge, setAge] = useState();
+    const [newGender, setGender] = useState();
+    const [newGithub, setGithub] = useState();
+    const [newLinkedin, setLinkedin] = useState();
 
     async function updateUserData(e) {
         e.preventDefault();
         await axios.patch(`${backend_url}/update_userdata/${user.accessToken}/${uid.id}`, {
             'about': newAbout,
             'name': newName,
+            'github':newGithub,
+            'linkedin':newLinkedin,
+            'age':newAge,
+            'gender':newGender
         }).then((e) => {
             console.log(e.data);
             window.location.reload();
@@ -27,7 +35,7 @@ function EditDashboard({ user, uid, about, displayName }) {
                         <span className='font-semibold'>Display name</span>
                     </label>
                     <div className='col-span-2'>
-                        <textarea className='resize-none p-2 w-[35vh]' defaultValue={displayName} value={newName} onChange={(e) => setNewName(e.target.value)}></textarea>
+                        <textarea className='resize-none p-2 w-[35vh] outline-none' defaultValue={displayName} value={newName} onChange={(e) => setNewName(e.target.value)}></textarea>
                     </div>
                 </div>
 
@@ -36,7 +44,25 @@ function EditDashboard({ user, uid, about, displayName }) {
                         <span className='font-semibold'>About me</span>
                     </label>
                     <div className='col-span-2'>
-                        <textarea className='resize-none p-2 w-[35vh] h-[40vh]' value={newAbout} defaultValue={about} onChange={(e) => setNewAbout(e.target.value)}></textarea>
+                        <textarea className='resize-none p-2 w-[35vh] h-[40vh] outline-none' value={newAbout} defaultValue={about} onChange={(e) => setNewAbout(e.target.value)}></textarea>
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                    <label className='flex flex-col'>
+                        <span className='font-semibold'>Age</span>
+                    </label>
+                    <div className='col-span-2'>
+                        <textarea className='resize-none p-2 w-[35vh] outline-none' defaultValue={age} value={newAge} onChange={(e) => setAge(e.target.value)}></textarea>
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                    <label className='flex flex-col'>
+                        <span className='font-semibold'>Gender</span>
+                    </label>
+                    <div className='col-span-2'>
+                        <textarea className='resize-none p-2 w-[35vh] outline-none' defaultValue={gender} value={newGender} onChange={(e) => setGender(e.target.value)}></textarea>
                     </div>
                 </div>
 
@@ -46,7 +72,7 @@ function EditDashboard({ user, uid, about, displayName }) {
                         <span className='text-xs'>(profile link)</span>
                     </label>
                     <div className='col-span-2'>
-                        <textarea className='resize-none p-2 w-[35vh]' defaultValue={displayName} value={newName} onChange={(e) => setNewName(e.target.value)}></textarea>
+                        <textarea className='resize-none p-2 w-[35vh] outline-none' defaultValue={github} value={newGithub} onChange={(e) => setGithub(e.target.value)}></textarea>
                     </div>
                 </div>
 
@@ -56,7 +82,7 @@ function EditDashboard({ user, uid, about, displayName }) {
                         <span className='text-xs'>(profile link)</span>
                     </label>
                     <div className='col-span-2'>
-                        <textarea className='resize-none p-2 w-[35vh]' defaultValue={displayName} value={newName} onChange={(e) => setNewName(e.target.value)}></textarea>
+                        <textarea className='resize-none p-2 w-[35vh] outline-none' defaultValue={linkedin} value={newLinkedin} onChange={(e) => setLinkedin(e.target.value)}></textarea>
                     </div>
                 </div>
 
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -8,6 +8,8 @@ import GigCard from './GigCard';
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import EditDashboard from '../Cards/EditDashboard';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Dashboard() {
 
@@ -19,6 +21,11 @@ export default function Dashboard() {
     const [email, setEmail] = useState();
     const [img, setimg] = useState();
     const [about, setAbout] = useState();
+    const [age, setAge] = useState();
+    const [gender, setGender] = useState();
+    const [github, setGithub] = useState();
+    const [linkedin, setLinkedin] = useState();
+
     const [mygigs, setmygigs] = useState('');
     const [showUpdate, setShowUpdate] = useState(false);
 
@@ -28,6 +35,10 @@ export default function Dashboard() {
             setdisplayName(userdata.data.name);
             setimg(userdata.data.picture);
             setAbout(userdata.data.about);
+            setGender(userdata.data.gender);
+            setAge(userdata.data.age);
+            setGithub(userdata.data.github);
+            setLinkedin(userdata.data.linkedin);
         })
 
         axios.get(`${backend_url}/mygigs/${uid.id}`).then((mygigsdata) => {
@@ -50,16 +61,32 @@ export default function Dashboard() {
                             <img className='rounded-full' src={img}></img>
                         </div>
                         <div>
-                            <p className='text-xl'>{displayName}</p>
+                            <p className='text-xl font-semibold'>{displayName}</p>
                         </div>
+
+                        <div className='px-4 flex space-x-4'>
+                            <p>Age : {age}</p>
+                            <p>Gender : {gender}</p>
+                        </div>
+
                         <div className='break-all'>
                             <p className=''>{email}</p>
                         </div>
+
+                        <div className='flex justify-center items-center space-x-3'>
+                            <div>
+                                <a href={github} target='_blank'><GitHubIcon /></a>
+                            </div>
+                            <div>
+                                <a href={linkedin} target='_blank'><LinkedInIcon /></a>
+                            </div>
+                        </div>
+
                         {
                             user && (
                                 <div className={`${user.uid === uid.id ? '' : 'hidden'}`}>
                                     <div className='flex justify-center items-center bg-lime-400 p-2 rounded-xl'>
-                                        <button onClick={() => {setTimeout(()=>{setShowUpdate(true)},400)}} className='font-semibold'><span className='flex justify-center items-center'><EditIcon /> Edit profile</span></button>
+                                        <button onClick={() => { setTimeout(() => { setShowUpdate(true) }, 400) }} className='font-semibold'><span className='flex justify-center items-center'><EditIcon /> Edit profile</span></button>
                                     </div>
                                 </div>
                             )
@@ -72,17 +99,22 @@ export default function Dashboard() {
                         <div className='p-4'>
                             <span className='text-2xl font-semibold'>About me</span>
                         </div>
+                        
                         <div className='p-4'>
                             <span>{about}</span>
                         </div>
 
-                        <div className={`${showUpdate ? '' : 'hidden'} flex flex-col justify-center items-center gap-y-3 p-2 fixed inset-0 bg-lime-200 z-10`}>
+                        <div className={`${showUpdate ? '' : 'hidden'} flex flex-col justify-start overflow-y-scroll items-center gap-y-3 p-2 fixed inset-0 bg-lime-200 z-10`}>
                             <div>
                                 <EditDashboard
                                     user={user}
                                     uid={uid}
                                     about={about}
                                     displayName={displayName}
+                                    age={age}
+                                    gender={gender}
+                                    github={github}
+                                    linkedin={linkedin}
                                 />
                             </div>
 

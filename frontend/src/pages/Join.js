3ac01198fa989552ea727ca0ae0a473d62d9eaa4
kamaldebnath <debnathk037@gmail.com>
2023-11-22 21:56:13 +0530

    latest

--- frontend/src/pages/Join.js
+++ frontend/src/pages/Join.js
@@ -9,27 +9,27 @@ import axios from 'axios';
 
 export default function Join() {
 
-  const backend_url='https://freelancerapp-wdtf.onrender.com';
-  
+  const backend_url = 'https://freelancerapp-wdtf.onrender.com';
+
   const [user, loading] = useAuthState(auth);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   async function google_login() {
     try {
       const result = await signInWithPopup(auth, new GoogleAuthProvider());
-      localStorage.setItem('token',result.user.accessToken);
-      axios.post(`${backend_url}/user_update/${result.user.accessToken}`,{
+      localStorage.setItem('token', result.user.accessToken);
+      axios.post(`${backend_url}/user_update/${result.user.accessToken}`, {
         'name': result.user.displayName,
         'picture': result.user.photoURL,
         'email': result.user.email,
         'email_verified': result.user.emailVerified,
         'uid': result.user.uid,
-        'about':'',
-        'age':'',
-        'gender':'',
-        'github':'',
-        'linkedin':''
-      }).then((e)=>{console.log(e)});
+        'about': '',
+        'age': '',
+        'gender': '',
+        'github': '',
+        'linkedin': ''
+      }).then((e) => { console.log(e) });
 
     } catch (error) {
       console.log(error);
@@ -37,25 +37,30 @@ export default function Join() {
   }
 
 
-  useEffect(()=>{
-    if(user){
+  useEffect(() => {
+    if (user) {
       navigate('/');
     }
   })
 
   return (
-    <div className='h-screen flex justify-center items-center'>
+    <div className='h-screen flex justify-center items-center bg-lime-50 font-display'>
       {!user && (
-        <div>
-          <button className='flex justify-around space-x-2 w-[30vh] bg-sky-400 p-3 rounded-full' onClick={google_login}>
+        <div className='w-[35vh] h-[40vh] flex flex-col gap-y-20 justify-center p-2 items-center bg-lime-300 rounded-xl border-4 border-black'>
+
+          <div className='text-3xl font-semibold underline'>
+            <span> Join Today</span>
+          </div>
+
+          <button className='flex justify-around space-x-2 w-[30vh] p-3 rounded-full border-2 border-black bg-white' onClick={google_login}>
             <div>
               <GoogleIcon />
             </div>
             <div className='font-semibold'>
               Sign in with Google
             </div>
-
           </button>
+
         </div>
       )}
 
